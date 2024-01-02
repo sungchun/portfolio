2eab68499e6e1eaaa7d17e7fa01b5504f922352d
Preston <preston.ng37@gmail.com>
2024-01-02 15:20:17 +0800

    add project content and update skill logo image paths

--- app/src/components/Skills.js
+++ app/src/components/Skills.js
@@ -4,77 +4,77 @@ function Skills() {
             <div>
                 <h3>Frontend</h3>
                 <div>
-                    <img alt="Javascript" src="app\src\assets\js.svg"/>
+                    <img alt="Javascript" src="app\src\assets\logos\js.svg"/>
                     <h4></h4>
                 </div>
                 <div>
-                    <img alt="HTML5" src="app\src\assets\html.svg"/>
+                    <img alt="HTML5" src="app\src\assets\logos\tml.svg"/>
                     <h4></h4>
                 </div>
                 <div>
-                    <img alt="CSS3" src=""/>
+                    <img alt="CSS3" src="app\src\assets\logos\css.svg"/>
                     <h4></h4>
                 </div>
                 <div>
-                    <img alt="React" src="app\src\assets\django.svg"/>
+                    <img alt="React" src="app\src\assets\logos\django.svg"/>
                     <h4></h4>
                 </div>
             </div>
             <div>
                 <h3>Backend</h3>
                 <div>
-                    <img alt="Python" src="app\src\assets\python.png"/>
+                    <img alt="Python" src="app\src\assets\logos\python.png"/>
                     <h4></h4>
                 </div>
                 <div>
-                    <img alt="Django" src="app\src\assets\django.svg"/>
+                    <img alt="Django" src="app\src\assets\logos\django.svg"/>
                     <h4></h4>
                 </div>
                 <div>
-                    <img alt="Node.js" src="app\src\assets\nodejs.svg"/>
+                    <img alt="Node.js" src="app\src\assets\logos\nodejs.svg"/>
                     <h4></h4>
                 </div>
                 <div>
-                    <img alt="MongoDB" src="app\src\assets\mongodb.svg"/>
+                    <img alt="MongoDB" src="app\src\assets\logos\mongodb.svg"/>
                     <h4></h4>
                 </div>
                 <div>
-                    <img alt="PostgreSQL" src="app\src\assets\postgresql.svg"/>
+                    <img alt="PostgreSQL" src="app\src\assets\logos\postgresql.svg"/>
                     <h4></h4>
                 </div>
             </div>
             <div>
                 <h3>Tools</h3>
                 <div>
-                    <img alt="Git" src="app\src\assets\git.svg"/>
+                    <img alt="Git" src="app\src\assets\logos\git.svg"/>
                     <h4></h4>
                 </div>
                 <div>
-                    <img alt="GitHub" src="app\src\assets\github.svg"/>
+                    <img alt="GitHub" src="app\src\assets\logos\github.svg"/>
                     <h4></h4>
                 </div>
                 <div>
-                    <img alt="Yarn" src="app\src\assets\yarn.svg"/>
+                    <img alt="Yarn" src="app\src\assets\logos\yarn.svg"/>
                     <h4></h4>
                 </div>
                 <div>
-                    <img alt="Npm" src="app\src\assets\npm.svg"/>
+                    <img alt="Npm" src="app\src\assets\logos\npm.svg"/>
                     <h4></h4>
                 </div>
                 <div>
-                    <img alt="VSCode" src="app\src\assets\vscode.svg"/>
+                    <img alt="VSCode" src="app\src\assets\logos\vscode.svg"/>
                     <h4></h4>
                 </div>
                 <div>
-                    <img alt="AWS" src="app\src\assets\aws.eps"/>
+                    <img alt="AWS" src="app\src\assets\logos\aws.eps"/>
                     <h4></h4>
                 </div>
                 <div>
-                    <img alt="Airflow" src="app\src\assets\airflow.svg"/>
+                    <img alt="Airflow" src="app\src\assets\logos\airflow.svg"/>
                     <h4></h4>
                 </div>
                 <div>
-                    <img alt="Bash" src="app\src\assets\bash.svg"/>
+                    <img alt="Bash" src="app\src\assets\logos\bash.svg"/>
                     <h4></h4>
                 </div>
             </div>
--- app/src/projects.js
+++ app/src/projects.js
@@ -1,30 +1,45 @@
 export const projects = {
     "shipwrecked": {
         "title": "Shipwrecked",
-        "image": "placeholder1",
-        "description":"placeholder2",
-        "link":"placeholder3",
+        "image": "app\src\assets\projects\shipwrecked.png",
+        "description":"Shipwrecked is a text-based RPG where the player’s environment is described to them with descriptive\
+        text, and the player types text commands to play the game.\
+        This project was very challenging as there were many problems that I had never faced before. In\
+        order to create this project, I designed a makeshift game engine to create the game's core\
+        mechanics. Afterward, all of the game content that was written just had to be plugged into the game.\
+        Other challenges that I faced were: creating the text-based interactivity, coding some of the\
+        commands such as 'COMBINE' and 'WALKTO', and creating the save feature.",
+        "link":"https://github.com/sungchun/shipwrecked",
         "technology":"placeholder4" 
      },
      "journi": {
         "title": "Journi",
-         "image":"placeholder5",
-         "description":"placeholder6",
-         "link":"placeholder7",
+         "image":"app\src\assets\projects\shipwrecked.png",
+         "description":"‘Journi’ is a social media site that allows its users to share posts about trips they made around the\
+         world. Users can view the locations of these posts on a map, follow other users, and comment on\
+         others’ posts.\
+         This project was completed in a group of 3, my job was to implement the Mapbox API to display an\
+         interactive map. Using the Mapbox documentation, I successfully created an interactive map component\
+         that displayed markers using geocoded addresses. It also includes a 'fly to' feature, where the user\
+         can fly the map to selected locations.",
+         "link":"https://github.com/sungchun/journi/blob/main/assets/journi-homepage.png",
          "technology":"placeholder8"
      },
      "mixtape": {
          "title": "Mixtape",
-         "image":"placeholder9",
-         "description":"placeholder10",
-         "link":"placeholder10",
+         "image":"app\src\assets\projects\mixtape-main.png",
+         "description":"This project was the second project I completed during my Software Engineering Immersive course at General Assembly.\
+         This project was a pair project and its objective was to make a web app that Utilized RESTful APIs to provide information.",
+         "link":"https://github.com/sungchun/mixtape",
          "technology":"placeholder11"
      },
      "circle destroyer": {
          "title": "Circle Destroyer",
-         "image":"placeholder12",
-         "description":"placeholder13",
-         "link":"placeholder14",
+         "image":"app\src\assets\projects\gameplay.gif",
+         "description":"This project was the first project I did during my Software Engineering Immersive course at General Assembly.\
+         This project began after my cohort and I had learned about JavaScript, HTML, and CSS. My project is a game that is hosted on a website \
+         where all of the mechanics and visuals are created using JavaScript, HTML, and CSS.",
+         "link":"https://github.com/sungchun/platformer",
          "technology":"placeholder15"
      }
      
