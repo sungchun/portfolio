2bfcee045451b99009380316dd0b7ae8fbc8ebfd
Preston <preston.ng37@gmail.com>
2024-01-02 14:39:38 +0800

    add assets to links component

--- app/src/components/Links.js
+++ app/src/components/Links.js
@@ -1,9 +1,9 @@
 function Links() {
     return (
         <div>
-            <a href=""><img/></a>
-            <a href=""><img/></a>
-            <a href=""><img/></a>
+            <a href="github.com/sungchun"><img src=""/></a>
+            <a href="https://www.linkedin.com/in/prestonsungchunng/"><img src="app\src\assets\linked.png"/></a>
+            <a href="mailto:preston.ng37@gmail.com"><img src="app\src\assets\email.png"/></a>
         </div>
     );
 }
